785f735c577860410b496b5f6b89a4a9cf7d6ce8
chandan895 <dablukumarpaswan1998@gmail.com>
2026-02-16 13:42:11 +0530

    add filel

---
+++ server.js
@@ -0,0 +1,72 @@
+
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
+
+
+
+
+const app=express();
+app.use(cors());
+app.use(express.json())
+
+const db=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"crud"
+})
+
+app.get('/', (req,res)=>{
+   const sql ="SELECT * FROM student";
+
+  db.query(sql,(err,result)=>{
+    if(err) return res.json({Message:"Error inside server."});
+    return res.json(result);
+   })
+})
+
+app.post('/student',(req,res)=>{
+    const sql="INSERT INTO student (`name`,`email`) VALUES (?)"
+    const values=[
+        req.body.name,
+        req.body.email
+    ]
+    db.query(sql,[values],(err,result)=>{
+        if(err) return res.json(err)
+            return res.json(result);
+    })
+})
+
+app.get('/read/:id', (req,res)=>{
+   const sql ="SELECT * FROM student WHERE id=?";
+   const id=req.params.id;
+
+  db.query(sql,[id],(err,result)=>{
+    if(err) return res.json({Message:"Error inside server."});
+    return res.json(result);
+   })
+})
+
+app.put('/update/:id',(req,res)=>{
+    const sql="UPDATE student SET `name`=?, `email`=? WHERE id=?";
+    const id=req.params.id;
+    db.query(sql,[req.body.name,req.body.email,id],(err,result)=>{
+        if(err) return res.body.json({Message:"Error inside server."});
+        return res.json(result);
+    })
+})
+
+
+app.delete('/delete/:id',(req,res)=>{
+    const sql="DELETE from student WHERE id=?";
+     const id=req.params.id;
+    db.query(sql,[id],(err,result)=>{
+        if(err) return res.body.json({Message:"Error inside server."});
+        return res.json(result);
+    })
+})
+
+app.listen(8001,()=>{
+    console.log("listening")
+});
